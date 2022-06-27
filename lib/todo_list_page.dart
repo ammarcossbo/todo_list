597ccf8a78de3deb18a00907cafe6e620e45da32
ammarcossbo <ammarcossbo@gmail.com>
2022-06-27 20:49:38 -0300

    final

--- lib/todo_list_page.dart
+++ lib/todo_list_page.dart
@@ -8,14 +8,74 @@ class TodoListPage extends StatefulWidget {
 }
 
 class _TodoListPageState extends State<TodoListPage> {
+  TextEditingController _textController = TextEditingController();
+
+  List<String> tarefas = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Todo List'),
+        title: const Text('Lista de Tarefas'),
       ),
-      body: Center(
-        child: Text('Todo List'),
+      body: Container(
+        padding: EdgeInsets.all(24),
+        child: Column(
+          children: [
+            TextField(
+              controller: _textController,
+            ),
+            Container(
+              height: 250,
+              width: 400,
+              child: ListView.separated(
+                separatorBuilder: (context, index) => Divider(),
+
+                // shrinkWrap: true,
+                itemCount: tarefas.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(tarefas[index]),
+                    onLongPress: () {
+                      setState(() {
+                        tarefas.removeAt(index);
+                      });
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            backgroundColor: Colors.green,
+            onPressed: () {
+              if (_textController.text.isNotEmpty) {
+                setState(() {
+                  tarefas.add(_textController.text);
+                });
+                _textController.clear();
+              }
+              print(tarefas);
+            },
+            child: const Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            backgroundColor: Colors.green,
+            onPressed: () {
+              setState(() {
+                tarefas = [];
+              });
+              _textController.clear();
+            },
+            child: const Icon(Icons.remove),
+          ),
+        ],
       ),
     );
   }
